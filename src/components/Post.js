d546ef00e2cc034b9444e11fc32ff059c7de23a8
barralaugusto <augustobarral@gmail.com>
2022-11-14 10:39:45 -0300

    Mejora en posteos home

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,38 @@
 import React, {Component} from "react";
-import {Text, View, TouchableOpacity, StyleSheet, TextInput, FlatList } from 'react-native';
-import { auth, db } from "../firebase/config";
-import firebase from "firebase";
+import {Text, View, TouchableOpacity, StyleSheet, TextInput, Image } from 'react-native';
 
 class Post extends Component {
     constructor(props) {
+        super(props);
         this.state = {
-           
+            description: "",
+            posteos: [],
+            loading: true,
+            comentario: ''
         }
     }
+
+    render(){
+        const styles = StyleSheet.create({
+            image: {
+                height: 200,
+            },
+            field: {
+                borderColor: '#dcdcdc',
+                borderWidth: 1,
+                borderRadius: 2,
+                padding: 3,
+                marginBottom: 8
+            },
+        })
+
+        return(
+            <View>
+                <Image style={styles.image} source={{ uri: this.props.photo }} resizeMode='contain' />
+                <Text>{this.props.post}</Text>
+            </View>
+        )
+    }
 }
+
+export default Post;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,6 @@
-import { CurrentRenderContext } from '@react-navigation/native';
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, Image, StyleSheet, TextInput, FlatList } from 'react-native';
-import TabContainter from '../components/TabContainer';
+import { View, Image, StyleSheet, TextInput, FlatList } from 'react-native';
+import Post from '../components/Post';
 import { auth, db } from '../firebase/config';
 
 
@@ -35,6 +34,18 @@ class Home extends Component {
         console.log(auth.currentUser)
     }
 
+    onComentar(){
+        db.collection('Comentarios').add({
+            owner: auth.currentUser.email,
+            createdAt: Date.now(),
+            comentario: this.state.comentario,
+        })
+        .then(()=> { 
+            this.props.navigation.navigate('Home')  
+})
+        .catch(e => console.log(e))
+}
+
     render() {
         const styles = StyleSheet.create({
             image: {
@@ -50,7 +61,7 @@ class Home extends Component {
                 padding: 3,
                 marginBottom: 8
             },
-            flatlist: {
+            list: {
                 width: '100%',
                 flex: 1
             },
@@ -60,17 +71,27 @@ class Home extends Component {
 
         return (
             <>
-                {this.state.loading ? <Image style={styles.loading} source={require('../images/Loading_icon.gif')}></Image> : <View style={styles.flatlist}>
-                    <FlatList data={this.state.posteos} keyExtractor={item => item.id.toString()}
-                        renderItem={({ item }) => <><Image style={styles.image} source={{ uri: item.data.photo }} resizeMode='contain' /><Text>{item.data.post}</Text> <TextInput
-                            style={styles.field}
-                            keyboardType='default'
-                            placeholder='Comentar'
-                            onChangeText={text => this.setState({ comentario: text })}
-                            value={this.state.comentario}
-                        /></>}
-                    />
-                </View>}
+                {this.state.loading ? <Image style={styles.loading} source={require('../images/Loading_icon.gif')}></Image> : 
+                
+                <View style={styles.list}>
+                    <FlatList data={this.state.posteos} keyExtractor={item => item.id.toString()} renderItem={({item})=> <Post post={item.data.post} photo = {item.data.photo}></Post> }/>
+                </View>
+
+                // <View style={styles.flatlist}>
+
+                    
+                //     <FlatList data={this.state.posteos} keyExtractor={item => item.id.toString()}
+                //         renderItem={({ item }) => <><Image style={styles.image} source={{ uri: item.data.photo }} resizeMode='contain' /><Text>{item.data.post}</Text> <TextInput
+                //             style={styles.field}
+                //             keyboardType='default'
+                //             placeholder='Comentar'
+                //             onChangeText={text => this.setState({ comentario: text })}
+                //             value={this.state.comentario}/>
+                //             <TouchableOpacity onPress = {()=>this.onComentar()}></TouchableOpacity>
+                //             </>}
+                //     />
+                // </View>
+                }
             </>
 
 
